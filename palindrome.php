9711cbc80d0a470afaf8ff88b13b28f949697ac1
Marino Di Clemente <kernelfolla@gmail.com>
2020-09-29 10:40:09 +0200

    lint php

--- palindrome.php
+++ palindrome.php
@@ -1,44 +1,51 @@
 <?php
 
-function splitString($a){
-   $len = strlen($a);
+function splitString($a)
+{
+    $len = strlen($a);
 
-   $start = substr($a, 0, $len/2);
-   $mid = $len % 2 ? substr($a, $len / 2 , 1) : null;
-   $end = substr($a, ($len/2) + $len % 2);
+    $start = substr($a, 0, $len / 2);
+    $mid   = $len % 2 ? substr($a, $len / 2, 1) : null;
+    $end   = substr($a, ($len / 2) + $len % 2);
 
-   $tmp = [$start, $mid, $end];
+    $tmp = [$start, $mid, $end];
 
-   $ret = [];
-   foreach($tmp as $val){
-     if($val !== null){
-         $ret[] = $val;
-     }
-   }
-   return $ret;
+    $ret = [];
+    foreach ($tmp as $val) {
+        if ($val !== null) {
+            $ret[] = $val;
+        }
+    }
+
+    return $ret;
 }
 
-function cleanString($str){
-    $str = str_replace(' ','',$str);
-    $str = str_replace('\'','',$str);
-    $str = str_replace('.','',$str);
+function cleanString($str)
+{
+    $str = str_replace(' ', '', $str);
+    $str = str_replace('\'', '', $str);
+    $str = str_replace('.', '', $str);
     $str = mb_strtolower($str);
-    $str = remove_accents($str); 
+    $str = remove_accents($str);
+
     return $str;
 }
 
-function isPalindrome($str){
-    $str = cleanString($str);
+function isPalindrome($str)
+{
+    $str      = cleanString($str);
     $splitted = splitString($str);
-    if(count($splitted) == 3){
+    if (count($splitted) == 3) {
         $splitted = [$splitted[0], $splitted[2]];
     }
     $splitted[1] = strrev($splitted[1]);
     var_dump($splitted);
+
     return $splitted[0] == $splitted[1];
 }
 
-function remove_accents($str){
+function remove_accents($str)
+{
     return str_replace(
         ['è'],
         ['e'],
